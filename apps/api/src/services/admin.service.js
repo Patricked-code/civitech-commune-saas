e87e7a082f7415eabf30f29dffb14e9c3199717b
Patricked-code <eric.diby@wealthtechinnovations.com>
2026-04-16 23:22:43 +0200

    feat(api): extend admin service with roles and user crud

--- apps/api/src/services/admin.service.js
+++ apps/api/src/services/admin.service.js
@@ -1,27 +1,33 @@
 const { listUsers } = require('../repositories/user.repository');
-const { createUser } = require('../repositories/user-write.repository');
+const { createUser, updateUserByEmail, deleteUserByEmail } = require('../repositories/user-write.repository');
+const { listRoles } = require('../repositories/role.repository');
 const { listProcedures } = require('../repositories/procedure.repository');
 const { createProcedure } = require('../repositories/procedure-write.repository');
 const { updateProcedureByCode, deleteProcedureByCode } = require('../repositories/procedure-admin.repository');
 const { listDossiers } = require('../repositories/dossier.repository');
 
 async function getAdminSnapshot() {
-  const [users, procedures, dossiers] = await Promise.all([
+  const [users, procedures, dossiers, roles] = await Promise.all([
     listUsers(),
     listProcedures(),
     listDossiers(),
+    listRoles(),
   ]);
 
   return {
     usersCount: users.length,
     proceduresCount: procedures.length,
     dossiersCount: dossiers.length,
+    rolesCount: roles.length,
   };
 }
 
 module.exports = {
   listUsers,
   createUser,
+  updateUserByEmail,
+  deleteUserByEmail,
+  listRoles,
   listProcedures,
   createProcedure,
   updateProcedureByCode,
