4f22f08c27dcd6aed196b19317fc4aff5b706a71
Patricked-code <eric.diby@wealthtechinnovations.com>
2026-04-16 16:00:39 +0200

    feat(api): add auth service

---
+++ apps/api/src/services/auth.service.js
@@ -0,0 +1,48 @@
+const crypto = require('crypto');
+const { users } = require('../data/authSeed');
+const { getPermissionsForRoles } = require('./rbac.service');
+
+const activeSessions = new Map();
+
+function sanitizeUser(user) {
+  return {
+    id: user.id,
+    tenantId: user.tenantId,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    roleCodes: user.roleCodes,
+    userType: user.userType,
+    permissions: getPermissionsForRoles(user.roleCodes),
+  };
+}
+
+function login(email, password) {
+  const user = users.find((item) => item.email === email && item.password === password);
+  if (!user) return null;
+  const token = crypto.randomBytes(24).toString('hex');
+  const publicUser = sanitizeUser(user);
+  activeSessions.set(token, publicUser);
+  return { token, user: publicUser };
+}
+
+function getSession(token) {
+  if (!token) return null;
+  return activeSessions.get(token) || null;
+}
+
+function logout(token) {
+  if (!token) return false;
+  return activeSessions.delete(token);
+}
+
+function listDemoUsers() {
+  return users.map((item) => sanitizeUser(item));
+}
+
+module.exports = {
+  login,
+  getSession,
+  logout,
+  listDemoUsers,
+};
